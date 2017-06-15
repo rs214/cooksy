7424105894cb3e7fa3f29ec1384115b3fb903feb
Robert Si <robertsi0214@gmail.com>
2017-06-14 17:47:43 -0700

    (feat) add post to action and reducer

--- client/src/actions/purchaseActions.js
+++ client/src/actions/purchaseActions.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { attachTokenToTheHeader } from '../utils/RequestHelper';
 
 export const GET_PURCHASES = 'GET_PURCHASES';
+export const POST_PURCHASE = 'POST_PURCHASE';
 
 export function getPurchases() {
   const headers = attachTokenToTheHeader();
@@ -10,6 +11,15 @@ export function getPurchases() {
 
   return {
     type: GET_PURCHASES,
+    payload: request
+  };
+}
+
+export function postPurchaseDetails(values) {
+  const request = axios.post('/api/users/purchases', values, { headers: headers });
+
+  return {
+    type: POST_PURCHASE,
     payload: request
   };
 }
